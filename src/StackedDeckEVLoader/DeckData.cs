85dff8bd0bec5c5344dbbaaa2759fd293349c6eb
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2022-09-14 18:00:51 -0600

    Initial Commit

---
+++ src/StackedDeckEVLoader/DeckData.cs
@@ -0,0 +1,55 @@
+﻿
+public class DeckData
+{
+    public Line[] lines { get; set; }
+    public Language language { get; set; }
+}
+
+public class Language
+{
+    public string name { get; set; }
+    public Translations translations { get; set; }
+}
+
+public class Translations
+{
+}
+
+public class Line
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string icon { get; set; }
+    public int stackSize { get; set; }
+    public string artFilename { get; set; }
+    public int itemClass { get; set; }
+    public Sparkline sparkline { get; set; }
+    public Lowconfidencesparkline lowConfidenceSparkline { get; set; }
+    public object[] implicitModifiers { get; set; }
+    public Explicitmodifier[] explicitModifiers { get; set; }
+    public string flavourText { get; set; }
+    public float chaosValue { get; set; }
+    public float exaltedValue { get; set; }
+    public float divineValue { get; set; }
+    public int count { get; set; }
+    public string detailsId { get; set; }
+    public int listingCount { get; set; }
+}
+
+public class Sparkline
+{
+    public float[] data { get; set; }
+    public float totalChange { get; set; }
+}
+
+public class Lowconfidencesparkline
+{
+    public float[] data { get; set; }
+    public float totalChange { get; set; }
+}
+
+public class Explicitmodifier
+{
+    public string text { get; set; }
+    public bool optional { get; set; }
+}
---
+++ src/StackedDeckEVLoader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+var uri = "https://poe.ninja/api/data/itemoverview?league=Kalandra&type=DivinationCard";
+using var client = new HttpClient();
+
+var resp = await client.GetAsync(uri);
+var contentStream = await resp.Content.ReadAsStreamAsync();
+var data = await JsonSerializer.DeserializeAsync<DeckData>(contentStream);
+
+if (data == null)
+{
+    throw new NullReferenceException(nameof(data));
+}
+
+var path = Path.GetFullPath("output.txt");
+Console.WriteLine($"Writing > {path}");
+
+using var file = File.Open(path, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+using var writer = new StreamWriter(file);
+writer.AutoFlush = true;
+
+foreach (var line in data.lines.OrderBy(l => l.name))
+{
+    var output = $"{line.name}\t{line.chaosValue}";
+    Console.WriteLine(output);
+    await writer.WriteLineAsync(output);
+}
+
+Console.WriteLine();
+Console.WriteLine($"Finished writing to {path}");
+Console.WriteLine("Execution complete, press enter to finish");
+Console.ReadLine();
